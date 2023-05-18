065663b6ed06bdb75b1294a2f439175fd26cd16b
student15 <student15@vkuint.ru>
2023-05-18 14:26:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число" + Environment.NewLine);
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number > 0)
+            {
+                Console.WriteLine($"Число положительное, {number + 5}");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine($"Число отрицательное, {number - 5}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
